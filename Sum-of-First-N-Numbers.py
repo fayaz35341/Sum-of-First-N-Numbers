c83ad8f3cc9254901b871716787e4d4e61b5a0eb
Riyaz shaik <94755295+fayaz35341@users.noreply.github.com>
2025-06-11 23:18:11 +0530

    Update Sum-of-First-N-Numbers.py

--- Sum-of-First-N-Numbers.py
+++ Sum-of-First-N-Numbers.py
@@ -16,3 +16,13 @@ class Solution:
         return int((n*(n+1))/2)
         
 print(Solution().seriesSum(5))
+
+#3
+
+class Solution:
+    def seriesSum(self, n : int) -> int:
+        # code here
+        if n==0:
+            return 0
+        return n+self.seriesSum(n-1)
+print(Solution().seriesSum(5))        
